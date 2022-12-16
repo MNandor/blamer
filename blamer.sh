9969d38263f77170bbc289fe58b3774a712f2b30
MNandor <mnandor@users.noreply.github.com>
2022-12-16 11:15:18 +0200

    Allow for toggling diff and file-at-the-time mode

--- blamer.sh
+++ blamer.sh
@@ -3,8 +3,11 @@
 commits=`git log --oneline $@ | sed 's/ .*//'`
 
 sel=0
+diff=''
 
 while true; do
+	clear
+
 	cur=0
 	for commit in $commits; do
 		if [ $cur == $sel ]; then
@@ -16,15 +19,29 @@ while true; do
 
 	commt=`echo $commits | sed 's/[a-z]/\u\0/' | cut -d' ' -f $((sel + 1))`
 
-	git show $commt $@
+	echo
 
-	echo -e "\n$commt - h/l to switch commit"
+	if [ -z "$diff" ]; then
+		git show $commt $@
+	elif [ "$diff" == ':' ]; then
+		git show $commt:$@
+	fi
+
+	echo -e "\n$diff$commt - h/l to switch commit"
 	read -n 1 -s key
 	if [ $key == 'l' ]; then
 		sel=$(($sel + 1))
 	fi
 	if [ $key == 'h' ]; then
 		sel=$(($sel - 1))
+	fi
+	if [ $key == 'd' ]; then
+		if [ -z "$diff" ]; then
+			diff=':'
+			echo hiiii
+		elif [ "$diff" == ':' ]; then
+			diff=''
+		fi
 	fi
 
 done
