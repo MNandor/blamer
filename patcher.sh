cca36244eb85334d21b3817f4470a8a89705328a
MNandor <mnandor@users.noreply.github.com>
2023-01-02 16:13:59 +0200

    Add color coding to patcher

--- patcher.sh
+++ patcher.sh
@@ -4,8 +4,10 @@ patches=`ls patches`
 
 for patch in $patches; do
 	echo $patch
-	git apply --check --reverse patches/$patch 2>/dev/null && echo Removable
-	git apply --check patches/$patch 2>/dev/null && echo Appliable
+	(git apply --check --reverse patches/$patch 2>/dev/null && echo -e "\033[1;32mApplied\033[0m") ||
+	(git apply --check patches/$patch 2>/dev/null && echo -e "\033[1;33mAppliable\033[0m") ||
+	echo -e "\033[1;31mBad\033[0m"
+
 
 done
 
