5f66c2d65cb31c99fb8f92acbb7ea00195b87018
MNandor <mnandor@users.noreply.github.com>
2023-01-02 16:15:33 +0200

    Colors are shown with patch file name

--- patcher.sh
+++ patcher.sh
@@ -3,11 +3,10 @@
 patches=`ls patches`
 
 for patch in $patches; do
-	echo $patch
-	(git apply --check --reverse patches/$patch 2>/dev/null && echo -e "\033[1;32mApplied\033[0m") ||
-	(git apply --check patches/$patch 2>/dev/null && echo -e "\033[1;33mAppliable\033[0m") ||
-	echo -e "\033[1;31mBad\033[0m"
-
+	(git apply --check --reverse patches/$patch 2>/dev/null && echo -e "\033[1;32m$patch\033[0m") ||
+	(git apply --check patches/$patch 2>/dev/null && echo -e "\033[1;33m$patch\033[0m") ||
+	echo -e "\033[1;31m$patch\033[0m"
 
 done
+	echo -e "\n\033[1;32mApplied\033[0m" "\033[1;33mAppliable\033[0m" "\033[1;31mBad\033[0m"
 
